3cdd3990864a01b18009e7808b3c35be65cd92b4
Max <dranik15@mail.ru>
2019-02-20 00:36:29 +0300

    Update WordsManipulation.cs

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
-using System;
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
 
 namespace PadawansTask8
 {
@@ -7,8 +9,26 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (text.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            StringBuilder str = new StringBuilder(text);
+            HashSet<string> words = new HashSet<string>(text.Split());
+            foreach (string word in words)
+            {
+                if (word.Length == 0)
+                {
+                    break;
+                }
+                int i = str.ToString().IndexOf(word) + word.Length;
+                str.Replace(word, "", i, str.Length - i);
+            }
+            text = str.ToString();
         }
     }
 }
